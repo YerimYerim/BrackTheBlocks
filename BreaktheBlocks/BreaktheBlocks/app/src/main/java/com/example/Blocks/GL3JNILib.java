68af17c58b4a953bc2482260d29abbd27ecb9018
yerim <vigvang_bip@naver.com>
2021-12-11 19:11:11 +0900

    add Opengl Frame Buffer draw success

---
+++ BreaktheBlocks/BreaktheBlocks/app/src/main/java/com/example/Blocks/GL3JNILib.java
@@ -0,0 +1,10 @@
+package com.example.Blocks;
+
+public class GL3JNILib {
+    static {
+        System.loadLibrary("Blocks");
+    }
+    public static native void Start(int width, int height);
+    public static native void Update();
+    public static native void InputEvent();
+}
--- BreaktheBlocks/BreaktheBlocks/app/src/main/java/com/example/Blocks/MainActivity.java
+++ BreaktheBlocks/BreaktheBlocks/app/src/main/java/com/example/Blocks/MainActivity.java
@@ -11,30 +11,22 @@ import com.example.Blocks.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'Blocks' library on application startup.
-    static {
-        System.loadLibrary("Blocks");
+
+    GL3JNIView mView;
+
+    @Override protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mView = new GL3JNIView(getApplication());
+        setContentView(mView);
     }
 
-    private ActivityMainBinding binding;
-    private View mGLView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-        setContentView(R.layout.activity_main);
-        mGLView = findViewById(R.id.gl_surfaceView);
-
+    @Override protected void onPause() {
+        super.onPause();
+        mView.onPause();
     }
 
-    /**
-     * A native method that is implemented by the 'Blocks' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    @Override protected void onResume() {
+        super.onResume();
+        mView.onResume();
+    }
 }
